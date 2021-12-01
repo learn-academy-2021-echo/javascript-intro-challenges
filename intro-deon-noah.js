919910be571155fa0b352f40bdbda4f86caee4f0
DeonFranklin <deondeefranklin@gmail.com>
2021-12-01 14:39:46 -0800

    intro-deon-noah.js

---
+++ intro-deon-noah.js
@@ -0,0 +1,20 @@
+console.log(34 +71);
+
+console.log(123 - 67);
+
+console.log(56 * 23);
+
+console.log(45 / 5);
+
+console.log(5 ** 7);
+
+console.log(33 % 6);
+
+var myName = "Deon"
+console.log(myName.length);
+
+console.log(myName.includes("e"));
+
+console.log(myName[0]);
+
+console.log(myName.toUpperCase());
