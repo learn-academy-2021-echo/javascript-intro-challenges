8f9e8bbee976cf777ce9db694c7943865c8aaba5
mikkajimenez <mikka.jimenez@gmail.com>
2021-12-01 14:43:48 -0800

    Mikka did first part of challenges

---
+++ intro-johnb-mikkaj.js
@@ -0,0 +1,29 @@
+// write code that will log the outcome of 34 added to 71
+console.log(34+71);
+
+//write code that will log the outcome of 67 subracted from 123
+console.log(123-67);
+
+//outcome of 56 * 23
+console.log(56*23);
+
+//outcome of 45/5
+console.log(45/5);
+
+//outcome of 5^7
+console.log(5**7);
+
+//outcome of remainder 33/6
+console.log(33%6);
+
+//length of a string containing my name
+console.log("Mikka Jimenez".length);
+
+//logs if it includes letter else
+console.log("Mikka Jimenez".includes("e"));
+
+// logs character at first index
+console.log("Mikka Jimenez".charAt(0));
+
+//logs string in all uppercas
+console.log("Mikka Jimenez".toUpperCase());
