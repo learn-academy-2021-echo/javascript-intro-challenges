8df533b4f2a48aca71fef5e380e6b48f0f155e8a
spiralcode28 <learnacademy@LEARNs-MacBook-Air.local>
2021-12-01 15:04:39 -0800

    JS Intro Solved lines 1 - 10

--- intro-hs-wd.js
+++ intro-hs-wd.js
@@ -1,1 +1,23 @@
 // Hannah and Warren's intro file
+
+console.log(1, 34 + 71);
+
+console.log(2, 123 - 67);
+
+console.log(3, 56 * 23);
+
+console.log(4, 45 / 5);
+
+console.log(5, 5 ** 7);
+
+console.log(6, 33 % 6);
+
+var myName = "Hannah"
+
+console.log(7, myName.length);
+
+console.log(8, myName.includes("e"));
+
+console.log(9, myName[0]);
+
+console.log(10, myName.toUpperCase());
