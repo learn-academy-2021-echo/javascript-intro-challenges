f9cd12fb9dc95a986771ad089c567d93887c824c
Jbudd512 <Jbudd0512@gmail.com>
2021-12-01 17:09:56 -0600

    John worked on problems 11-20

--- intro-johnb-mikkaj.js
+++ intro-johnb-mikkaj.js
@@ -1,29 +1,59 @@
 // write code that will log the outcome of 34 added to 71
-console.log(34+71);
+console.log(1, 34+71);
 
 //write code that will log the outcome of 67 subracted from 123
-console.log(123-67);
+console.log(2, 123-67);
 
 //outcome of 56 * 23
-console.log(56*23);
+console.log(3, 56*23);
 
 //outcome of 45/5
-console.log(45/5);
+console.log(4, 45/5);
 
 //outcome of 5^7
-console.log(5**7);
+console.log(5, 5**7);
 
 //outcome of remainder 33/6
-console.log(33%6);
+console.log(6, 33%6);
 
 //length of a string containing my name
-console.log("Mikka Jimenez".length);
+console.log(7, "Mikka Jimenez".length);
 
 //logs if it includes letter else
-console.log("Mikka Jimenez".includes("e"));
+console.log(8, "Mikka Jimenez".includes("e"));
 
 // logs character at first index
-console.log("Mikka Jimenez".charAt(0));
+console.log(9, "Mikka Jimenez".charAt(0));
 
 //logs string in all uppercas
-console.log("Mikka Jimenez".toUpperCase());
+console.log(10, "Mikka Jimenez".toUpperCase());
+
+//logs true or false depending on if 34/3 is greater than 67/2
+console.log(11, 34/3 > 67/2);
+
+//logs true or false depending on if 5 evaluates to the same as "5"
+console.log(12, 5 == "5");
+
+//logs true or false depending on if 5 strictly equals "5"
+console.log(13, 5 === "5");
+
+//logs true or false depending on if !3 strictly equals to 3
+console.log(14, !3 === 3);
+
+//logs true or false depending on if "LEARN".length is strictly equal to 5 and if "Student".length strictly equals 7
+console.log(15, "LEARN".length === 5 && "Student".length === 7);
+
+//logs true or false depending on if "LEARN".length is strictly equal to 5 or if "Student".length is strictly equal to 10
+console.log(16, "LEARN".length === 5 || "Student".length === 10);
+
+//logs true or false depending on if "LEARN" contains the subset "RN"
+console.log(17, "LEARN".includes("RN"));
+
+//logs true or false depending on if "LEARN" contains the subset "rn"
+console.log(18, "LEARN".includes("rn"));
+
+//logs true or false depending on if "LEARN" at index 0 equals to "l"
+console.log(19, "LEARN"[0] === "l");
+
+//Modify the code from the previous question to return true
+console.log(20, "LEARN"[0] !== "l");
