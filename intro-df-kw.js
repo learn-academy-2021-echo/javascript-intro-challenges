216f5bc9874429c88be750f8a99de68d33c209c5
LEARN Academy <ford.py@gmail.com>
2021-12-01 14:47:16 -0800

    df-changes

--- intro-df-kw.js
+++ intro-df-kw.js
@@ -10,14 +10,21 @@ console.log(123 - 67);
 console.log(56 * 23);
 
 // Write the code that will log the outcome of 45 divided by 5.
+console.log(45 / 5);
 // Write the code that will log the outcome of 5 to the power of 7.
+console.log(5 ** 7);
 // Write the code that will log the whole number remainder of 33 divided by 6.
+console.log(33 % 6);
 // Write the code that will log the length of a string containing your name.
+var myName = "Demetrius"
+console.log(myName.length);
 // Write the code that will log whether your string includes the letter "e"?
+console.log(myName.includes("e"));
 // Write the code that will log the character at the first index of the string.
+console.log(myName[0]);
 // Write the code that will log the string in all uppercase letters.
+console.log(myName.toUpperCase());
 // Write the code that will log true or false for the following:
-
 // Is 34 divided by 3 greater than 67 divided by 2?
 // Does 5 evaluate to the same as "5"?
 // Does 5 strictly equal "5"?
