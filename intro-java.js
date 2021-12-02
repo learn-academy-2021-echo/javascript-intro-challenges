17c1530b859e461801d716a560b04942aa3c07b0
gluay88 <p.wantagaew@gmail.com>
2021-12-02 12:02:33 -0800

    XXXX

---
+++ intro-java.js
@@ -0,0 +1,22 @@
+var price = 110;
+if(price <= 100){
+console.log("In budget");
+} else {
+    console.log("Nahh!");
+}
+
+var hungry = false;
+if(hungry === true){
+console.log("eat food");
+} else {
+    console.log("keep coding");
+}
+
+var trfLights = "yellow";
+if(trfLights === "green"){
+    console.log("Go!");
+} else if (trfLights === "yellow"){
+    console.log("Slow down!");
+} else {
+    console.log("Stop");
+}
