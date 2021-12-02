62e13713ca2fc20561ba53d4471d9232163a2b19
LEARN Academy <ford.py@gmail.com>
2021-12-02 13:17:49 -0800

    First 3 challenges done.

--- conditionals-DF-RA.js
+++ conditionals-DF-RA.js
@@ -4,11 +4,30 @@
 // Make sure you try different options and change the variables to ensure properly working code.
 //
 // Write a statement that takes a variable of item and logs "in budget" if a price is $100 or less.
-//
+const item = 100;
+
+if (item == 100 || item < 100){
+  console.log("in budget");
+}
 // Write a statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
-//
+const hungry = false
+
+if (hungry) {
+  console.log("Eat Food");
+} else {
+  console.log("Keep Coding");
+}
 // Write a statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
-//
+const trafficLight = "green";
+
+if (trafficLight == "green"){
+  console.log("go");
+} else if (trafficLight == "yellow") {
+  console.log("slow down");
+} else {
+  console.log("stop");
+}
+
 // Write a statement that takes two variables that are numbers and outputs the larger number. If the numbers are equal, output "the numbers are the same".
 //
 // Write a statement that takes a variable of a number and logs whether the number is odd, even, or zero.
