d2a016cf84889f5273f0f3544849403eba19908d
gluay88 <p.wantagaew@gmail.com>
2021-12-02 13:17:36 -0800

    Another Condinals

--- intro-java.js
+++ intro-java.js
@@ -20,3 +20,28 @@ if(trfLights === "green"){
 } else {
     console.log("Stop");
 }
+
+var num1 = "Antonio";
+var num2 = 90;
+if(num1 > num2){
+    console.log(`${num1} is greather than ${num2}`);
+} else if (num2 > num1){
+    console.log(`${num2} is greather than ${num1}`);
+} else if (num1 === num2){
+    console.log(`${num1} equals ${num2}`);
+
+} else {
+    console.log("Try again!");
+}
+
+var numbers = 91;
+if(numbers === 0){
+    console.log("ZERO");
+} else if(numbers % 2 === 0){
+    console.log("Even number");
+} else if (numbers % 2 !== 0){
+    console.log("ODD");
+} else {
+    console.log("sth. else");
+}
+ 
