de5cb7947bf164be4fd2bcdab2cd1a98b51daa9d
LEARN Academy <jeffreysjoshua@gmail.com>
2021-12-03 11:42:32 -0800

    Josh and Yann loops

--- arrays-josh-warren.js
+++ arrays-josh-warren.js
@@ -5,8 +5,8 @@ groceryList.push("soda")
 console.log(groceryList)
 // Write the code that will add "granola" to the end of array without altering the original array.
 // !!!
-var iable = groceryList.push("granola")
-console.log(groceryList);
+var iable = groceryList.concat("granola")
+console.log(iable);
 //
 // Write the code that will return a subset of the array containing only "chips" and "dip".
 console.log(groceryList.slice(0,2));
