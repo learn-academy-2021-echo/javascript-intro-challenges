293749d4d892d2b5a724b5c60febdf8d9d764bdb
gluay88 <p.wantagaew@gmail.com>
2021-12-02 17:21:54 -0800

    updated ver.2

--- js-arrays.js
+++ js-arrays.js
@@ -24,3 +24,19 @@ console.log(groceryList);
 
 // Gluay was here!
 // But Antonio was here first!
+
+
+// Write the code that will add the number 0 to the beginning of the array.
+var numbers = [2, 4, 6, 8, 10]
+numbers.unshift(0);
+console.log(numbers);
+// Write the code that will add the number 12 to the end of the array.
+numbers.push(12);
+console.log(numbers);
+
+// Write the code that will remove the first number from the array.
+
+// Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
+
+
+// New stufffffff Gluay
