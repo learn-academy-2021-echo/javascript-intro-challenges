7a4c240f67c21f38f21ff07812edb5336989f411
Frank Carbonero <frankcarbonero@icloud.com>
2021-12-02 16:44:44 -0800

    awnsered question

--- array-noah-frank.js
+++ array-noah-frank.js
@@ -26,15 +26,21 @@ console.log(snacks);
 
 // Consider the variable:
 //
-// var numbers = [2, 4, 6, 8, 10]
+var numbers = [2, 4, 6, 8, 10]
+numbers.unshift(0)
+console.log(numbers)
 // Write the code that will add the number 0 to the beginning of the array.
-//
+numbers.push(12)
+console.log(numbers)
 // Write the code that will add the number 12 to the end of the array.
 //
 // Write the code that will remove the first number from the array.
-//
+numbers.shift()
+console.log(numbers)
+
 // Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
-//
+var newNumbers=[0]
+console.log(newNumbers.concat(numbers))
 // Consider the variable:
 //
 // var numSet = [2, 13, 6, 8, 4, 2]
