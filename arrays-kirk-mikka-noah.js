6c1138b28b9437876045b1e1ee79b808915f1732
mikkajimenez <mikka.jimenez@gmail.com>
2021-12-02 16:35:36 -0800

    Mikka did challenges 19 and 20

--- arrays-kirk-mikka-noah.js
+++ arrays-kirk-mikka-noah.js
@@ -88,3 +88,14 @@ console.log(cohortNames2.reverse());
 let allNames = cohortNames1.concat(cohortNames2);
 allNames.sort();
 console.log(allNames);
+
+var numbers = [42, 221, 71, 7, 18, 87]
+var oddIndexes = []
+// Write the code that logs the values from the numbers array that are at odd indexes.
+for (var i = 1 ; i < numbers.length + 1 ; i+=2){
+    console.log(numbers[i]);
+    oddIndexes.push(numbers[i]);
+}
+
+// Write the code that adds the values from odd indexes into the oddIndexes array.
+console.log(oddIndexes);
