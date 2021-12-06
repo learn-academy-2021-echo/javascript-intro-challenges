4f79f267745b8d79b5d20b90f92156712e87d6ce
WWDavidson <w.w.davidson33@gmail.com>
2021-12-06 16:16:11 -0600

    continued working functions challenge'

--- functions-wd-bs.js
+++ functions-wd-bs.js
@@ -1,4 +1,25 @@
-const "marco" = () => {
-  return "polo"
+// //Write a function named marco that returns "polo".
+//
+// const marco = () => {
+//   return "polo"
+// }
+// console.log(marco("polo"));
+//
+// //Write a function named greeting that takes a name as an argument and returns "Welcome, <person's name here>!"
+//
+// const greeting = (name) => {
+//   return 'Welcome, ${name}'
+// }
+// console.log(greeting('Welcome, ${name}'));
+//
+
+// //Write a function named oddOrEven that takes a number as an argument and returns whether the number is odd or even.
+var number = 4
+const oddOrEven = () => {
+if (number % 2 === 0){
+return "The number is even."
+} else {
+  return "The number is odd."
 }
-console.log(("polo"))
+}
+console.log(oddOrEven(number));
