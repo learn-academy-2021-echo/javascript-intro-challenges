7f1e72fc33120af8c754e492e3d3a93ebb446ac9
Samanthap803b <samanthap803b@gmail.com>
2021-12-06 13:07:27 -0800

    functions challenge

---
+++ functions-Mikka-Sam.js
@@ -0,0 +1,31 @@
+const marco= ()=> {
+  return "polo"
+}
+//
+// console.log(marco())
+
+const greeting = (name)=> {
+  return `Welcome, ${name}!`
+}
+// console.log(greeting("Samantha"))
+
+const oddOrEven =(number)=> {
+if (number % 2 === 0){
+  return `number ${number} is even`
+}
+else
+ return `number ${number} is odd`
+}
+// console.log(oddOrEven (3))
+// console.log(oddOrEven (4))
+
+ const triple = (num1,num2) => {
+  return num1 * num2
+}
+  // console.log(triple(5,3))
+
+
+ const multiply = (num1,num2)=>{
+return num1 * num2
+}
+console.log(multiply(4,5))
